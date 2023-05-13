62a477a288e7b08e4827fe52fb58c5a00395f5d1
Martin Kiragu <mkiragu@componentsense.com>
2023-05-13 12:33:04 +0300

    added migrations and database tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,8 @@ class User extends Authenticatable  implements LaratrustUser
     protected $fillable = [
         'name',
         'email',
+        'location',
+        'phone',
         'password',
     ];
 
--- database/migrations/2023_05_13_092646_create_supermarkets_table.php
+++ database/migrations/2023_05_13_092646_create_supermarkets_table.php
@@ -4,21 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('supermarkets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone')->unique();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('location');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
         });
@@ -29,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('supermarkets');
     }
 };
